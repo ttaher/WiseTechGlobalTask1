50d2e0e8d501f816411a9b1f8ef50a7e073926ad
ttaher <talat.t.mohammed@gmail.com>
2018-12-08 12:41:29 +0300

    done

--- WiseTechGlobalTask1/Solution.cs
+++ WiseTechGlobalTask1/Solution.cs
@@ -17,14 +17,10 @@ namespace WiseTechGlobalTask1
         public static  int solution(int[] ranks)
         {
             var count = 0;
-            // write your code in C# 6.0 with .NET 4.5 (Mono)
-            int[] postive = ranks.Select(i => i).Where(x => x > 0).OrderBy(j => j).ToArray();
             for (int i = 0; i < ranks.Length; i++)
             {
                 if (ranks.Select(m => m).Where(x => (ranks[i]+1) == x).ToList().Count>0)
-                {
                     count++;
-                }
             }
             return count;
         }
